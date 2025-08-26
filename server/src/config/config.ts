922eb583cae892fafc9cf261b548a77cf7ae4776
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-08-26 11:54:25 +0530

    feat(config): add initial configuration file with env variables
    Initialize config file to centralize environment variables access

---
+++ server/src/config/config.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const _config = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+};
+
+const config = Object.freeze(_config);
+
+export default config;
