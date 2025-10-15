af7838f50721e3d72cbf6510924d45c8d4711a87
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-10-15 13:37:36 +0530

    fix(TradeActivitiesTable): update column header from BDA to crop/Qty

--- client/src/app/(Dashboard)/components/TradeActivitiesTable.tsx
+++ client/src/app/(Dashboard)/components/TradeActivitiesTable.tsx
@@ -31,7 +31,7 @@ const TradeActivitiesTable: React.FC<TradeActivitiesTableProps> = ({
               scope="col"
               className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
             >
-              BDA
+              crop/Qty
             </th>
             <th
               scope="col"
