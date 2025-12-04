14f1c8654d546a40a4d2305f5261190de619bd97
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-12-04 14:58:34 +0530

    feat(cropExcel): add new crop data table page with filtering and pagination

---
+++ client/src/app/cropExcel/page.tsx
@@ -0,0 +1,305 @@
+import React from "react";
+import Image from "next/image";
+
+const page = () => {
+  const crops = [
+    {
+      _id: {
+        $oid: "6911b65ef06056d7e95d28b6",
+      },
+      name: "Cotton",
+      category: {
+        name: "Fibre Crops",
+        image: "/uploads/Fiber crops.png",
+      },
+      location: {
+        latitude: 0,
+        longitude: 0,
+        city: "Amarawati",
+        state: "Maharashtra",
+      },
+      supplyDemand: {
+        arrivalQtyToday: 55,
+        stockAvailability: 0,
+        majorArrivalDistricts: ["Amarawati"],
+      },
+      marketLocation: "Not Specified",
+      cropInsights: {
+        season: "Kharif Season",
+        sowingTime: "",
+        harvestTime: "",
+        averageYield: "",
+        requiredTemperature: "",
+        waterRequirement: "",
+      },
+      farmerInformation: {
+        recommendedSellingTime: "",
+        storageTips: "",
+        qualityGrading: "",
+      },
+      additionalDetails: {
+        govtMSP: null,
+        exportImportStatus: "",
+        subsidiesSchemes: [],
+      },
+      variants: [
+        {
+          name: "Cotton",
+          image: "/uploads/cotton.jpg",
+          price: 5250,
+          minPrice: 5000,
+          maxPrice: 5550,
+        },
+      ],
+      otherDetails: {
+        reportedDate: {
+          $date: "2020-01-01T00:00:00.000Z",
+        },
+        rawExcelData: {
+          State: "Maharashtra",
+          District: "Amarawati",
+          "Market ": "Varud",
+          Season: "Kharif Season",
+          "Crop Name": "Cotton",
+          Category: "Fibre Crops",
+          "Arrivals (Tonnes)": 55,
+          "Min Price (Rs./Quintal)": 5000,
+          "Max Price (Rs./Quintal)": 5550,
+          "Modal Price (Rs./Quintal)": 5250,
+          "Reported Date": 43831,
+        },
+      },
+    },
+  ];
+
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md mt-4">
+      <div className="flex justify-between items-center mb-4">
+        <div className="flex items-center gap-2">
+          <Image
+            src="/Images/Vector.png"
+            alt="Onboarding"
+            width={24}
+            height={24}
+            className=""
+          />
+          <h2 className="text-lg font-semibold">All Crops</h2>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="flex flex-col">
+            <span className="text-sm font-medium text-gray-600">Category</span>
+            <div className="relative">
+              <select className="border border-gray-300 rounded-md p-2 text-sm appearance-none pr-20">
+                <option> All</option>
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm font-medium text-gray-600">Sort by</span>
+            <div className="relative">
+              <select className="border border-gray-300 rounded-md p-2 text-sm appearance-none pr-20">
+                <option> All</option>
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                Crop Name
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                Category
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                City
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                State
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                Price (Rs./Quintal)
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {crops.map((crop) => (
+              <tr key={crop._id.$oid}>
+                <td className="px-6 py-4">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0 h-10 w-10">
+                      <Image
+                        className="h-10 w-10 rounded-full object-cover"
+                        height={0}
+                        width={0}
+                        sizes="100vw"
+                        src={
+                          crop.variants[0]?.image || "/Images/default-crop.jpg"
+                        }
+                        alt={crop.name}
+                      />
+                    </div>
+                    <div className="ml-4">
+                      <div className="text-sm font-medium text-gray-900">
+                        {crop.name}
+                      </div>
+                      <div className="text-sm text-gray-500">
+                        {crop._id.$oid}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td className="px-6 py-4">
+                  <div className="text-sm text-center text-gray-900">
+                    {crop.category.name}
+                  </div>
+                </td>
+                <td className="px-6 py-4 text-center">
+                  <span className="px-2 inline-flex text-xs text-center leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    {crop.location.city}
+                  </span>
+                </td>
+                <td className="px-6 py-4 text-sm text-center text-gray-500">
+                  {crop.location.state}
+                </td>
+                <td className="px-6 py-4 text-sm text-center text-gray-500">
+                  {crop.variants[0]?.price}
+                </td>
+                <td className="px-6 py-4 text-sm text-center font-medium flex items-center justify-center gap-4">
+                  <a
+                    href="#"
+                    className="text-zinc-600 border border-zinc-600 hover:text-white hover:bg-zinc-600 px-3 py-1 rounded-md"
+                  >
+                    Details
+                  </a>
+                  <a
+                    href="#"
+                    className="text-green-600 border border-green-600 hover:text-white hover:bg-green-500 px-3 py-1 rounded-md"
+                  >
+                    Update
+                  </a>
+                  <a
+                    href="#"
+                    className="text-red-600 border border-red-600 hover:text-white hover:bg-red-600 px-3 py-1 rounded-md"
+                  >
+                    Delete
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="flex justify-center mt-4">
+        <nav
+          className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+          aria-label="Pagination"
+        >
+          <a
+            href="#"
+            className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+          >
+            <span className="sr-only">Previous</span>
+            <svg
+              className="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+          <a
+            href="#"
+            aria-current="page"
+            className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+          >
+            1
+          </a>
+          <a
+            href="#"
+            className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+          >
+            2
+          </a>
+          <a
+            href="#"
+            className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+          >
+            3
+          </a>
+          <a
+            href="#"
+            className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+          >
+            <span className="sr-only">Next</span>
+            <svg
+              className="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default page;
